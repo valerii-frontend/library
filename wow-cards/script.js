475ad60ee03d9b1fea32cf83f3336dd14d7d121e
Valerii from earth <lvaldev.code@gmail.com>
2022-07-17 16:15:57 +0200

    change images paths

--- wow-cards/script.js
+++ wow-cards/script.js
@@ -1,6 +1,6 @@
 const cards = document.querySelectorAll(".cards__card");
 const cardsWrapper = document.querySelector(".cards");
-document.querySelector("audio").volume = 0.2;
+document.querySelector("audio").volume = 0.05;
 
 cards.forEach((card, index) => {
 	card.addEventListener("click", function (e) {
@@ -17,6 +17,6 @@ function removeActiveClasses() {
 }
 
 const bodyBg = (index) => {
-	cardsWrapper.style.backgroundImage = `url(../img/load${index}.jpg)`;
+	cardsWrapper.style.backgroundImage = `url(./img/load${index}.jpg)`;
 	cardsWrapper.classList.add("active");
 };
