fa47cf795e7b34dc358676b01213438795a9223a
Valerii from earth <lvaldev.code@gmail.com>
2022-07-17 15:21:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const cards = document.querySelectorAll(".cards__card");
+const cardsWrapper = document.querySelector(".cards");
+document.querySelector("audio").volume = 0.2;
+
+cards.forEach((card, index) => {
+	card.addEventListener("click", function (e) {
+		removeActiveClasses();
+		bodyBg(index);
+		card.classList.add("cards__card_active");
+	});
+});
+function removeActiveClasses() {
+	cards.forEach((card) => {
+		card.classList.remove("cards__card_active");
+		cardsWrapper.classList.remove("active");
+	});
+}
+
+const bodyBg = (index) => {
+	cardsWrapper.style.backgroundImage = `url(../img/load${index}.jpg)`;
+	cardsWrapper.classList.add("active");
+};
